d2de1154816105e8713f4058f9e7ef5aab365259
nzhou@iastate.edu <nzhou@iastate.edu>
2019-10-18 16:25:42 -0500

    organize header files

--- src/init.c
+++ src/init.c
@@ -1,10 +1,8 @@
-#include "add.h"
-#include "dm_call.h"
+#include "PhiMRF.h"
 #include <R_ext/Rdynload.h>
 
 
 void R_init_PhiMRF(DllInfo *info) {
-  R_RegisterCCallable("PhiMRF", "add",  (DL_FUNC) &add_);
   R_RegisterCCallable("PhiMRF", "pmrf",  (DL_FUNC) &double_metropolis_cont);
   R_RegisterCCallable("PhiMRF", "preprocess_big",  (DL_FUNC) &preprocess);
 
